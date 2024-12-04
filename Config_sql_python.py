72e25d24bb8c8b7e2c4bbae35cdd9c4517c45984
João Victor Borges de Oliveira <jv.borgesoliveira@outlook.com>
2024-12-04 11:53:04 -0300

    Create Config_sql_python.py

---
+++ Config_sql_python.py
@@ -0,0 +1,18 @@
+server = ''
+database = ''
+username = ''
+password = ''
+
+conn_str = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+def simpleQuery(query):
+    try:
+        conn = pyodbc.connect(conn_str)
+        
+        data = pd.read_sql(query, conn)
+        
+        conn.close()
+
+        return pd.DataFrame(data)
+    except Exception as e:
+        print(f'Error: {e}')
